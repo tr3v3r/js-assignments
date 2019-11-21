2569bfb80b29c5e69c21897c041bb457389788b8
Алексей Субоч <suboch.1991@mail.ru>
2019-11-21 22:24:31 +0300

    solve/homework task numbers 3&4

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -54,7 +54,7 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-    throw new Error('Not implemented');
+    return value1 /2 + value2 / 2 ;
 }
 
 /**
@@ -73,7 +73,7 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-    throw new Error('Not implemented');
+    return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
 }
 
 /**
